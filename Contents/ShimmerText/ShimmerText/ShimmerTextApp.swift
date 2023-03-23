b10437b1ce081b2d688f5ed3e9b45df2e01b6241
chminipark <chminipark@gmail.com>
2023-03-23 20:46:53 +0900

    Add README.md

--- Contents/ShimmerText/ShimmerText/ShimmerTextApp.swift
+++ Contents/ShimmerText/ShimmerText/ShimmerTextApp.swift
@@ -5,13 +5,18 @@
 //  Created by minii on 2023/03/22.
 //
 
+// Chip : Apple M1
+// MacOS : Ventura 13.2.1 (22D68)
+// Xcode Version : 14.2 (14C18)
+// Swift version : 5.7.2
+
 import SwiftUI
 
 @main
 struct ShimmerTextApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
---
+++ README.md
@@ -0,0 +1,19 @@
+# swiftui-animation-challenge
+
+## Introduction
+SwiftUI animation tutorials
+
+## Example apps
+|![ShimmerText](https://user-images.githubusercontent.com/77793412/227191694-6b049313-1052-49fc-88f3-5c6006d553e7.gif) Shimmer Effect|내용|설명|
+|---|---|---|
+|테스트1|테스트2|테스트3|
+|테스트1|테스트2|테스트3|
+|테스트1|테스트2|테스트3|
+
+
+
+
+
+
+
+
